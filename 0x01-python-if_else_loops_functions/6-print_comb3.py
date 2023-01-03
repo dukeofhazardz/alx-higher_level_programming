98a282f74c1c2ce34e9ec8a4af3e7c73704244ef
Daniel John <dj71286@gmail.com>
2023-01-03 23:06:10 +0000

    Added more files

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''A program that prints all possible different combinations of two digits.
+   01 and 10 are considered the same combination of the two digits 0 and 1'''
+
+for number1 in range(0, 10):
+    for number2 in range(number1 + 1, 10):
+        if number1 == 8 and number2 == 9:
+            print(f"{number1}{number2}")
+        else:
+            print(f"{number1}{number2}", end=", ")
