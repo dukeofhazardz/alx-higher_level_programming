1b62df3871ee2185e696970a6c977837fd9898ac
Daniel John <dj71286@gmail.com>
2023-01-03 23:42:59 +0000

    Final commit

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''A program that prints the ASCII alphabet, in reverse order,
+    alternating lowercase and uppercase'''
+i = 0
+for letter in range(ord('z'), ord('a') - 1, - 1):
+    print("{}".format(chr(letter - i)), end="")
+    i = 32 if i == 0 else 0
