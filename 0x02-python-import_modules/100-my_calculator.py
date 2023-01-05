6e832d89fe8e46f28ebfded42eb444a749ebae29
Daniel John <dj71286@gmail.com>
2023-01-05 17:59:05 +0000

    Tired

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,19 +5,25 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    length = len(sys.argv) - 1
+    length = len(sys.argv)
     a = int(sys.argv[1])
-    b = int(sys.argv[3])i
-    c = sys.argv[2]
+    b = int(sys.argv[3])
+    ops = sys.argv[2]
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-
-    if length != 3:
+    if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>", end="\n")
         sys.exit(1)
 
-    if c not in list(ops.keys()):
+    if ops in "+-*/":
+        if ops == "+":
+            print("{} + {} = {}".format(a, b, add(a, b)), end="\n")
+        elif ops == "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)), end="\n")
+        elif ops == "*":
+            print("{} * {} = {}".format(a, b, mul(a, b)), end="\n")
+        elif ops == "/":
+            print("{} / {} = {}".format(a, b, div(a, b)), end="\n")
+
+    else:
         print("Unknown operator. Available operators: +, -, * and /", end="\n")
         sys.exit(1)
-
-    print("{} {} {} = {}".format(a, c, b, ops[c](a, b)), end="\n")
