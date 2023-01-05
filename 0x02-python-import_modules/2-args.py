0d77d756626c8da6e43398d3d75180fce8783ea2
Daniel John <dj71286@gmail.com>
2023-01-05 12:05:25 +0000

    Final fix Hopefully

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
 
     length = len(sys.argv) - 1
 
-    if length == 1:
-        print("{} arguement:".format(length), end="\n")
+    if length == 0:
+        print("{} arguements.".format(length), end="\n")
 
-    elif length == 0:
-        print("0 arguements.", end="\n")
+    elif length == 1:
+        print("{} arguement:".format(length), end="\n")
 
     else:
         print("{} arguements:".format(length, end="\n"))
