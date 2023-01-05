f243306a494cb5d0d1a42f9bc55190a4195488c0
Daniel John <dj71286@gmail.com>
2023-01-05 12:02:42 +0000

    Latest fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         print("{} arguement:".format(length), end="\n")
 
     elif length == 0:
-        print("{} arguements.", end="\n")
+        print("0 arguements.", end="\n")
 
     else:
         print("{} arguements:".format(length, end="\n"))
