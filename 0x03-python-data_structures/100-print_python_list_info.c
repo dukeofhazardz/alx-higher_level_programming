740e3b8e21a619ffceb78501c9cb643a6a951e22
Daniel John <dj71286@gmail.com>
2023-01-08 21:17:56 +0000

    Added CPython file

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - A C function that prints
+ *		some basic info about Python lists.
+ * @p: Py Object to be printed
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, i;
+	PyListObject *obj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	print("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
