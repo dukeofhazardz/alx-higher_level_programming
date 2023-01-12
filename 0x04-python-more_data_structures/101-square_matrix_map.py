4cc36833dd4076e3ab1c4850499b1a9229d0e0da
Daniel John <dj71286@gmail.com>
2023-01-12 03:08:39 +0000

    Advanced square matrix fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-"""A function that computes the square value
-    of all integers of a matrix using map"""
-
-
 def square_matrix_map(matrix=[]):
     return (list(map(lambda x: list(map(lambda y: y*y, x)), matrix)))
