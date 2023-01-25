ab73e1ef893b47d58d795c876e43e205d1f84f63
Daniel John <dj71286@gmail.com>
2023-01-25 00:24:13 +0000

    Little Fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
+
+
 class Square:
     """A class Square that defines a square"""
     def __init__(self, size):
         """Initializes a Square
 
         Args:
-            size (int): The size of the new square"""
+            size (int): The size of the new square
+        """
         self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
+
+
 class Square:
     """A class Square that defines a square"""
 
