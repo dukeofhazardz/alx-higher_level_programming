86c5117bf4d9b31116866244056499ca21467133
Daniel John <dj71286@gmail.com>
2023-02-01 17:44:31 +0000

    Another fix

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
         rect = []
         for i in range(self.__height):
             [rect.append("#") for j in range(self.__width)]
-            if i != self.__height:
+            if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
 
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -60,7 +60,7 @@ class Rectangle:
         rect = []
         for i in range(self.__height):
             [rect.append(str(self.print_symbol)) for j in range(self.__width)]
-            if i != self.__height:
+            if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
 
