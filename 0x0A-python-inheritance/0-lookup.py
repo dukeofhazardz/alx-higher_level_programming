42ac8ead970af276a37fb54caa10feeb787f0a24
Daniel John <dj71286@gmail.com>
2023-02-07 17:40:45 +0000

    another fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines an inherited list class MyList."""
 
+
 def lookup(obj):
     """A Function that returns the list of available
         attritbutes and methods of an object"""
