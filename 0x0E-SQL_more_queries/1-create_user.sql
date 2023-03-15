8726b11f5d79fd79d8a3d4daf44d136f4783fec2
Daniel John <dj71286@gmail.com>
2023-03-15 22:40:49 +0000

    Hot fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 -- A script that creates the MySQL server user user_0d_1.
-SET GLOBAL validate_password.policy = 0;
-CREATE USER IF NOT EXISTS
-'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER
+IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-	ON *.*
-	TO 'user_0d_1'@'localhost';
+ON *.*
+TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
