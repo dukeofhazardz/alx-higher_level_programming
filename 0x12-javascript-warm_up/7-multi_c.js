fd4a07e81a906409968d8075a069ce5d0153abe1
Daniel John <dj71286@gmail.com>
2023-04-11 20:40:53 +0000

    Tackled more mandatory tasks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,9 +7,7 @@
 const count = parseInt(process.argv.slice(2));
 
 if (count && Number.isInteger(count)) {
-	for (let i = 0; i < count; i++) {
-		console.log('C is fun');
-	}
-}
-else
-	console.log('Missing number of occurrences');
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+} else { console.log('Missing number of occurrences'); }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,8 +4,8 @@
  * A script that prints the addition of 2 integers
  */
 
-function add(a, b) {
-	return parseInt(a) + parseInt(b);
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
 }
 
 const arg = process.argv.slice(2);
