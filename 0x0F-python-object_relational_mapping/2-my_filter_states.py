d32206611926beee03367e9d93604cadbc190e2e
Daniel John <dj71286@gmail.com>
2023-04-17 00:34:49 +0100

    fixed bug at 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute(f"SELECT * FROM states WHERE name = '{name}' \
+    cursor.execute(f"SELECT * FROM states WHERE BINARY name = '{name}' \
                     ORDER BY id ASC")
     states = cursor.fetchall()
 
