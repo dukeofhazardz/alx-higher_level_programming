9363902ed646d81a344a9729e7ef98d5e5184a73
Daniel John <dj71286@gmail.com>
2023-04-27 09:53:36 -0700

    first python network task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-# A Bash script that takes in a URL, sends a request to that URL, and
-# displays the size of the body of the response in bytes
-
+# A Bash script that takes in a URL,displays the size of the response in bytes
 curl -s "$1" | wc -c
