7e65adb7049945d405bdf202f69aaba2e66c6ba5
Daniel John <dj71286@gmail.com>
2023-04-28 21:42:22 +0100

    more requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A Python script that takes in a URL and an email address, sends a POST
+    request to the passed URL with the email as a parameter, and finally
+    displays the body of the response. """
+
+if __name__ == "__main__":
+
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    data = {'email': sys.argv[2]}
+
+    req = requests.post(url, data=data)
+    print(req.text)
