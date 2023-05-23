31c2b8131d121ee4b55cf9e55301af5887660345
Daniel John <dj71286@gmail.com>
2023-05-23 21:47:45 +0100

    finished advanced tasks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-/* A script that prints all characters of a Star Wars movie: */
+/* A script that prints all characters of a Star Wars movie:
+ * Displaying one character name by line in the same order of
+ * the list “characters” in the /films/ response
+ */
 
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
@@ -11,6 +14,7 @@ request(url, function (error, response, body) {
     console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
+    const promises = [];
     for (const character of characters) {
       request(character, function (error, response, body) {
         if (!error) {
@@ -18,5 +22,8 @@ request(url, function (error, response, body) {
         }
       });
     }
+    Promise.all(promises).then((a) => {
+      for (const i of a) { console.log(i); }
+    });
   }
 });
